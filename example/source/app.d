675120b4c38e7f9a223278c495de1ac6b4e43984
deviator <code.viator@gmail.com>
2019-05-05 17:35:30 +0300

    improve example

--- example/source/app.d
+++ example/source/app.d
@@ -1,21 +1,55 @@
 import std.stdio;
+import std.format : format;
 import core.thread;
 
 import mosquittod;
 
-void main()
+int main(string[] args)
 {
+    enum topic = "mqtt/example";
+
     auto cli = new MosquittoClient();
 
     cli.connect();
 
-    // for test: mosquitto_pub -t mqtt/example -m "hello mqtt"
-    cli.subscribe("mqtt/example", (const(ubyte)[] data)
-    { writefln("get: %s", cast(const(char[]))data); }, 1);
-
-    while (true)
+    if (args.length > 1 && args[1] == "bombardire")
     {
-        cli.loop();
-        Thread.sleep(1.msecs);
+        foreach (i; 0 .. 100)
+        {
+            cli.publish(topic~format("/%d", i), format("msg %d", i), 1);
+            Thread.sleep(10.msecs);
+        }
+        Thread.sleep(10.msecs);
+        cli.publish(topic, "halt", 1);
+
+        return 0;
+    }
+    else if (args.length == 1)
+    {
+        bool run = true;
+
+        // for test: mosquitto_pub -t mqtt/example -m "hello mqtt"
+        cli.subscribe(topic ~ "/+", 2, (const(ubyte)[] data)
+        {
+            auto sdata = cast(const(char[]))data;
+            writefln("get: %s", sdata);
+            if (sdata == "halt") run = false;
+        });
+
+        while (run)
+        {
+            cli.loop();
+            Thread.sleep(1.msecs);
+        }
+        return 0;
+    }
+    else
+    {
+        stderr.writefln(
+            "unknown args %2$s\n"~
+            "use for monitor: %1$s\n"~
+            "use for bombardire: %1$s bombardire",
+            args[0], args[1..$]);
+        return 1;
     }
 }
